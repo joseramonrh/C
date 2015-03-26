296905b6257d2c937b0b004ab9995803917f3449
joseramonrh <a00397868@itesm.mx>
2015-03-26 02:10:21 -0600

    lk

--- mergesort.c
+++ mergesort.c
@@ -46,7 +46,7 @@ void Split(int A[], int B[], int inicio, int final){
 	}
 
 	int medio = (final-inicio)/2;
-	printf("%d", medio);
+	printf("%d", final);
 	Split(A, B, medio, final);
 	Split(A, B, inicio, medio);
 

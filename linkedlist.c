cbdd6a3ebcc3d3c4ba1643731af3fa4cd0d24d31
joseramonrh <a00397868@itesm.mx>
2015-03-25 22:24:00 -0600

    ofuc

--- linkedlist.c
+++ linkedlist.c
@@ -22,7 +22,7 @@ struct arista
 	arista *arista;
 	vertice *vertice;
 };
-arista
+
 void agregaArista(vertice * vertices, int grado, int probabilidad){
 	vertice * primerNodo = vertices;
 	vertice * cambianteNodo;

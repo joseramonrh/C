36c8c7a3980e26b072050c9b1cd031a1a33d3830
joseramonrh <a00397868@itesm.mx>
2015-03-26 22:09:35 -0600

    ,

--- sort.c
+++ sort.c
@@ -58,7 +58,7 @@ void Split(int A[], int B[], int inicio, int final);
             C[i] = random;
             D[i] = 0;
         }
-        heap(A,n);
+        //heap(A,n);
         Sort(A,n,0);
         MergeSort(C, D, n);
         count++;
@@ -80,7 +80,6 @@ void Split(int A[], int B[], int inicio, int final);
         valor =  first[i];
         hijo = i;
         raiz = (hijo-1)/2;
-        gcountHeap++;
         while(hijo > 0 && first[raiz] < valor){
             first[hijo] = first[raiz];
             hijo = raiz;
